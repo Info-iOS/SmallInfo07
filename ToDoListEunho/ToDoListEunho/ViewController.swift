4b80ba7c9937c2cc67f16529c5fb983c5dc62869
eunho0922 <tylereunholee010203@gmail.com>
2023-04-30 21:57:48 +0900

    toDoList 오류수정

--- ToDoListEunho/ToDoListEunho/ViewController.swift
+++ ToDoListEunho/ToDoListEunho/ViewController.swift
@@ -10,51 +10,51 @@ import UIKit
 import Then
 import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     private lazy var toDoListTableView = UITableView().then {
         $0.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
     }
     
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
+        
         nav()
         
-       view.addSubview(toDoListTableView)
+        toDoListTableView.delegate = self
+        toDoListTableView.dataSource = self
+        
+        view.addSubview(toDoListTableView)
         
         toDoListTableView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         
     }
-         
-   
     
     func nav() {
         let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
         navigationItem.rightBarButtonItem = rightBarButtonItem
-
+        
         let leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: nil)
-        navigationItem.leftBarButtonItem = leftBarButtonItem 
+        navigationItem.leftBarButtonItem = leftBarButtonItem
     }
-}
     
-    func TableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
         cell.textLabel?.text = "to do list 제목"
         cell.detailTextLabel?.text = "to do list 내용"
         cell.selectionStyle = .none
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
         
     }
-   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 5
-       
-  }
-
+}
 
      
 
